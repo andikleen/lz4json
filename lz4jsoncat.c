c739b35437721c925a5a0a733e252917d54fb6ef
Aaron Lichtman <aaronlichtman@gmail.com>
2023-09-16 01:54:16 -0700

    Make `decompress_jsonlz4_file` callable from external programs
    Temp

--- lz4jsoncat.c
+++ lz4jsoncat.c
@@ -20,14 +20,14 @@
    https://dxr.mozilla.org/mozilla-central/source/toolkit/components/lz4/lz4.js
  */
 #include <errno.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/fcntl.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
-#include <sys/fcntl.h>
-#include <stdio.h>
 #include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdint.h>
 #ifndef __APPLE__
 #include <endian.h>
 #else
@@ -36,59 +36,65 @@
 
 #include "lz4.h"
 
-int main(int argc, char **argv)
-{
-	if(argc == 2 && ((strcmp(argv[1], "--help")==0) || strcmp(argv[1], "-h")==0)) {
-		fprintf(stderr, "Usage: %s [file...]\n", argv[0]);
-		fprintf(stderr, "Decompresses mozilla style lz4json files.\nhttps://github.com/andikleen/lz4json/\n");
-		return 0;
-	}
-	while (*++argv) {
-		int fd = open(*argv, O_RDONLY);
-		if (fd < 0) {
-			perror(*argv);
-			continue;
-		}
-		struct stat st;
-		if (fstat(fd, &st) < 0) {
-			perror(*argv);
-			exit(1);
-		}
-		if (st.st_size < 12) {
-			fprintf(stderr, "%s: file too short\n", *argv);
-			exit(1);
-		}
+/**
+ * Decompress a mozilla style jsonlz4 file to stdout
+ */
+void decompress_jsonlz4_file(char *filepath) {
+    int fd = open(filepath, O_RDONLY);
+    if (fd < 0) {
+        perror(filepath);
+        return;
+    }
+    struct stat st;
+    if (fstat(fd, &st) < 0) {
+        perror(filepath);
+        exit(1);
+    }
+    if (st.st_size < 12) {
+        fprintf(stderr, "%s: file too short\n", filepath);
+        exit(1);
+    }
 
-		char *map = mmap(NULL, st.st_size, PROT_READ, MAP_SHARED, fd, 0);
-		if (map == (char *)-1) {
-			perror(*argv);
-			exit(1);
-		}
-		if (memcmp(map, "mozLz40", 8)) {
-			fprintf(stderr, "%s: not a mozLZ4a file\n", *argv);
-			exit(1);
-		}
-		size_t outsz = htole32(*(uint32_t *) (map + 8));
-		char *out = malloc(outsz);
-		if (!out) {
-			fprintf(stderr, "Cannot allocate memory\n");
-			exit(1);
-		}
-		if (LZ4_decompress_safe_partial(map + 12, out, st.st_size - 12, outsz, outsz) < 0) {
-			fprintf(stderr, "%s: decompression error\n", *argv);
-			exit(1);
-		}
-		ssize_t decsz = write(1, out, outsz);
-		if (decsz < 0 || decsz != outsz) {
-			if (decsz >= 0)
-				errno = EIO;
-			perror("write");
-			exit(1);
-		}
-		free(out);
-		munmap(map, st.st_size);
-		close(fd);
+    char *map = mmap(NULL, st.st_size, PROT_READ, MAP_SHARED, fd, 0);
+    if (map == (char *)-1) {
+        perror(filepath);
+        exit(1);
+    }
+    if (memcmp(map, "mozLz40", 8)) {
+        fprintf(stderr, "%s: not a mozLZ4a file\n", filepath);
+        exit(1);
+    }
+    size_t outsz = htole32(*(uint32_t *)(map + 8));
+    char *out = malloc(outsz);
+    if (!out) {
+        fprintf(stderr, "Cannot allocate memory\n");
+        exit(1);
+    }
+    if (LZ4_decompress_safe_partial(map + 12, out, st.st_size - 12, outsz, outsz) < 0) {
+        fprintf(stderr, "%s: decompression error\n", filepath);
+        exit(1);
+    }
+    ssize_t decsz = write(STDOUT_FILENO, out, outsz);
+    if (decsz < 0 || decsz != outsz) {
+        if (decsz >= 0) {
+            errno = EIO;
+        }
+        perror("Error writing file to stdout!\n");
+        exit(1);
+    }
+    free(out);
+    munmap(map, st.st_size);
+    close(fd);
+}
 
-	}
-	return 0;
+int main(int argc, char **argv) {
+    if (argc == 2 && ((strcmp(argv[1], "--help") == 0) || strcmp(argv[1], "-h") == 0)) {
+        fprintf(stderr, "Usage: %s [file...]\n", argv[0]);
+        fprintf(stderr, "Decompresses mozilla style lz4json files.\nhttps://github.com/andikleen/lz4json/\n");
+        return 0;
+    }
+    while (*++argv) {
+        decompress_jsonlz4_file(*argv);
+    }
+    return 0;
 }
